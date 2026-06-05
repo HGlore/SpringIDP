c7c0881183d2f2d83362c0d193971ef6a248230a
Harv <Harv@IDC10-SW08>
2026-06-05 10:12:56 +0800

    20260605 9:50 AM

---
+++ src/main/java/com/idp/SpringIDP/dto/AssignedResult.java
@@ -0,0 +1,13 @@
+package com.idp.SpringIDP.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AssignedResult {
+    private int imageCount;
+    private String msgResponse;
+}
